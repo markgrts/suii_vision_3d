6bd03820ea1a4644826d3624b4d4a81536c4dec0
markgrts <mark.geraets@student.fontys.nl>
2019-09-25 13:48:57 +0200

    feat: added filters_downsampling

---
+++ src_test_folders/filters/filters_downsampling/build/CMakeFiles/down_sampling.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/down_sampling.dir/d_sample.cpp.o"
+  "down_sampling.pdb"
+  "down_sampling"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/down_sampling.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src_test_folders/filters/filters_downsampling/d_sample.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <pcl/filters/voxel_grid.h>
+
+int main (int argc, char** argv) //Add filename of file that you want to open
+{
+  if (argc != 2)
+    return (0);
+
+  pcl::PCLPointCloud2::Ptr cloud (new pcl::PCLPointCloud2 ());
+  pcl::PCLPointCloud2::Ptr cloud_filtered (new pcl::PCLPointCloud2 ());
+
+  // Fill in the cloud data
+  pcl::PCDReader reader;
+  // Replace the path below with the path where you saved your file
+  reader.read (argv[1] , *cloud); // Remember to download the file first!
+
+  std::cerr << "PointCloud before filtering: " << cloud->width * cloud->height 
+       << " data points (" << pcl::getFieldsList (*cloud) << ").";
+
+  // Create the filtering object
+  pcl::VoxelGrid<pcl::PCLPointCloud2> sor;
+  sor.setInputCloud (cloud);
+  sor.setLeafSize (0.002f, 0.002f, 0.002f);
+  sor.filter (*cloud_filtered);
+
+  std::cerr << "PointCloud after filtering: " << cloud_filtered->width * cloud_filtered->height 
+       << " data points (" << pcl::getFieldsList (*cloud_filtered) << ").";
+
+  //add dsampled_ to filename that gets saved
+  std::string s;
+  s.append("dsampled_");
+  s.append(argv[1]);
+
+  pcl::PCDWriter writer;
+  writer.write ( s, *cloud_filtered, 
+         Eigen::Vector4f::Zero (), Eigen::Quaternionf::Identity (), false);
+
+  return (0);
+}

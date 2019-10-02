2422dd8627339945a5c8fd457b047d530028a7fd
markgrts <mark.geraets@student.fontys.nl>
2019-10-02 15:53:50 +0200

    Added object_detection

---
+++ src_test_folders/object_detection/color_pc/build/CMakeFiles/color_pc.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/color_pc.dir/color_pc_2.cpp.o"
+  "color_pc.pdb"
+  "color_pc"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/color_pc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src_test_folders/object_detection/roi_pc/build/CMakeFiles/roi_pc.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/roi_pc.dir/roi_pc.cpp.o"
+  "roi_pc.pdb"
+  "roi_pc"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/roi_pc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
